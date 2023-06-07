e530c55bb0c90b0d6f003e59ad8ee7ccbfedbad1
Victor Carmona <victor181851@gmail.com>
2023-06-07 17:38:37 -0600

    feat: The first functions were added.
    A function was added for players to enter their name and also the number to guess was hidden for player 2.

---
+++ multiplayer_guess_number.py
@@ -0,0 +1,20 @@
+import getpass
+print("Player 1, please enter your name")
+player_1 = input()
+
+print("Player 2, please enter your name")
+player_2 = input()
+
+print("")
+print(player_1,":")
+number_to_guess = getpass.getpass("Enter the number to guess")
+
+
+attempts = 100
+
+   
+
+
+
+    
+    
