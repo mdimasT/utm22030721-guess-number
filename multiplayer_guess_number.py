2939215460999a84778e64364d6cae6e8d5bdd67
Victor Carmona <victor181851@gmail.com>
2023-06-07 19:53:54 -0600

    feat: New functionalities were added.
    Added the while loop to allow player two to make guesses. If the guess is correct and there are remaining guesses, the player wins. If the guesses reach 10, the player loses. In addition, messages are added to inform the player of the outcome of each guess and the remaining points are updated.

--- multiplayer_guess_number.py
+++ multiplayer_guess_number.py
@@ -1,20 +1,46 @@
+#code libraries that will be used later.
 import getpass
+import os
+
+#Player one is asked for his name.
 print("Player 1, please enter your name")
 player_1 = input()
 
+print("")
+#Player two is asked for his name.
 print("Player 2, please enter your name")
 player_2 = input()
 
 print("")
 print(player_1,":")
-number_to_guess = getpass.getpass("Enter the number to guess")
-
+#Player one is asked for the number to guess.
+number_to_guess = int(getpass.getpass("Enter the number to guess"))
+print("")
 
 attempts = 100
 
-   
+#start of the loop that will allow player two to guess.
+while True:
+ 
+    print(player_2,":")
+    print("Guess the number")
+    number = int(input())
+    print("")
 
+    if number == number_to_guess and attempts != 0:
+        print("")
+        print("Congratulations,",player_2," you have won")
+        print("Remaining points:",attempts)
+        break
 
+    elif attempts == 10 :
+        print("")
+        print("No points remaining, you have lost", player_2)
+        print("The correct number was: ",number_to_guess)
+        break
 
-    
-    
+    else:
+        print("")
+        print("You have lost 10 points, try it again")
+        attempts = attempts - 10
+        print("You have ",attempts, "points left")
