f610bd8312ff0eac79aee1959a4c6e393379db31
MaxDimas <maxdimas@outlook.com>
2023-06-07 22:28:53 -0600

    fix: the input validation has been improved

--- guess_number.py
+++ guess_number.py
@@ -3,20 +3,22 @@ import random
 while True:
     print("")
     print("Enter the first number")
-    fn = int(input())
-    print("")
-    print("Enter the second number")
-    sn = int(input())
-
-    if fn % 1 == 0 and fn < sn and fn != str and sn != str:
-        break
-
-    else:
+    try:
+        fn = int(input())
         print("")
-        print("Remember the first number has to have a higher value than the second one and exclude decimals")
+        print("Enter the second number")
+        sn = int(input())
 
-randomNumber = random.randint(fn,sn)
+        if fn < sn:
+            break
+        else:
+            print("")
+            print("Remember the first number has to have a higher value than the second one")
+    except ValueError:
+        print("")
+        print("Invalid input. Please enter valid numbers.")
 
+randomNumber = random.randint(fn, sn)
 attempts = 100
 
 print(randomNumber)
@@ -24,29 +26,25 @@ print(randomNumber)
 while True:
     print("")
     print("Guess the number:")
-    number = int(input())
-    
+    try:
+        number = int(input())
 
-    if number == randomNumber and attempts != 0:
+        if number == randomNumber and attempts != 0:
+            print("")
+            print("Congratulations!!, you won with", attempts, "points")
+            break
+
+        elif attempts == 0:
+            print("")
+            print("No points remaining, you have lost")
+            print("The correct number was:", randomNumber)
+            break
+
+        else:
+            print("")
+            print("You have lost 10 points, try again")
+            attempts -= 10
+            print("You have", attempts, "points left")
+    except ValueError:
         print("")
-        print("Congratulations!!, you won with", attempts, "points")
-        
-        break
-
-    elif attempts == 10:
-        print("")
-        print("No points remaining, you have lost")
-        print("The correct number was: ",randomNumber)
-        break
-
-    else:
-        print("")
-        print("You have lost 10 points, try it again")
-        attempts = attempts - 10
-        print("You have ",attempts, "points left")
-   
-
-
-
-    
-    
+        print("Invalid input. Please enter a valid number.")
