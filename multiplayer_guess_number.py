3831f53e844e3ff29a6d456dc44d3364f1e1d3e4
Victor Carmona <victor181851@gmail.com>
2023-06-07 20:15:27 -0600

    featAdded a loop to ask players if they would like to play again.
    After the completion of the game, players are prompted if they want to play again. If the user inputs NO the program breaks out of the loop and ends.

--- multiplayer_guess_number.py
+++ multiplayer_guess_number.py
@@ -1,46 +1,55 @@
 #code libraries that will be used later.
 import getpass
-import os
 
-#Player one is asked for his name.
-print("Player 1, please enter your name")
-player_1 = input()
-
-print("")
-#Player two is asked for his name.
-print("Player 2, please enter your name")
-player_2 = input()
-
-print("")
-print(player_1,":")
-#Player one is asked for the number to guess.
-number_to_guess = int(getpass.getpass("Enter the number to guess"))
-print("")
-
-attempts = 100
-
-#start of the loop that will allow player two to guess.
+#Start of loop asking players if they would like to play again.
 while True:
- 
-    print(player_2,":")
-    print("Guess the number")
-    number = int(input())
+    
+    #Player one is asked for his name.
+    print("Player 1, please enter your name")
+    player_1 = input()
+
+    print("")
+    #Player two is asked for his name.
+    print("Player 2, please enter your name")
+    player_2 = input()
+
+    print("")
+    print(player_1,":")
+    #Player one is asked for the number to guess.
+    number_to_guess = int(getpass.getpass("Enter the number to guess"))
     print("")
 
-    if number == number_to_guess and attempts != 0:
-        print("")
-        print("Congratulations,",player_2," you have won")
-        print("Remaining points:",attempts)
-        break
+    attempts = 100
 
-    elif attempts == 10 :
+    #start of the loop that will allow player two to guess.
+    while True:
+    
+        print(player_2,":")
+        print("Guess the number")
+        number = int(input())
         print("")
-        print("No points remaining, you have lost", player_2)
-        print("The correct number was: ",number_to_guess)
-        break
 
-    else:
-        print("")
-        print("You have lost 10 points, try it again")
-        attempts = attempts - 10
-        print("You have ",attempts, "points left")
+        if number == number_to_guess and attempts != 0:
+            print("")
+            print("Congratulations,",player_2," you have won")
+            print("Remaining points:",attempts)
+            break
+
+        elif attempts == 10 :
+            print("")
+            print("No points remaining, you have lost", player_2)
+            print("The correct number was: ",number_to_guess)
+            break
+
+        else:
+            print("")
+            print("You have lost 10 points, try it again")
+            attempts = attempts - 10
+            print("You have ",attempts, "points left")
+    
+    print("")
+    print("Would you like to play again?")
+    again  = input()
+
+    if again.upper() == "NO":
+        break
